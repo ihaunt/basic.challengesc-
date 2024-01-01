70f5d7c0708d6f3e184ad85dd0956cffbbe18fb4
Jennifer Machado <dev.jenni@outlook.com>
2024-01-01 03:25:24 +0000

    classe hero

---
+++ classe-game/hero.js
@@ -0,0 +1,56 @@
+
+
+
+let hero = {
+    Name: 'Monk',
+    Age: 24,
+
+    type: {
+        0: ['Warrior', 'Espada'],
+        1: ['Wizard', 'Magia'],
+        2: ['Monk', 'Artes marciais'],
+        3: ['Ninja', 'Shuriken'],
+    }
+}
+
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+        this.ataque = '';
+
+        for (let index in hero.type) {
+            if (hero.type[index][0] === tipo) {
+                [this.tipo, this.ataque] = hero.type[index];
+                break;
+            }
+        }
+    }
+
+    atacar() {
+        let ataque;
+
+        switch (this.tipo) {
+            case 'Wizard':
+                ataque = 'usou magia';
+                break;
+            case 'Warrior':
+                ataque = 'usou espada';
+                break;
+            case 'Monk':
+                ataque = 'usou artes marciais';
+                break;
+            case 'Ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'ataque desconhecido';
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Exemplo de uso
+let meuHeroi = new Heroi('Herói A', 30, 'Warrior');
+meuHeroi.atacar();
